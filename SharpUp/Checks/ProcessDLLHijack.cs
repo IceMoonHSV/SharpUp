2b7ea633265ce5b1798315fe8f6565e2ff86e59e
IceMoonHSV <62567946+IceMoonHSV@users.noreply.github.com>
2022-04-09 07:47:38 -0700

    Processdllhijack (#2)
    * Added Process DLL Hijack
    
    * Updated ProcessDLLHijack to fit project requirements
    
    * Update ProcessDLLHijack.cs
    
    Indentation issue I was not able to fix in VS
    
    * Added file permission check. Updated message with warning for C:\Program Files. Changed output messages.

--- SharpUp/Checks/ProcessDLLHijack.cs
+++ SharpUp/Checks/ProcessDLLHijack.cs
@@ -1,11 +1,13 @@
 ﻿using SharpUp.Classes;
 using System;
+using System.IO;
 using static SharpUp.Utilities.RegistryUtils;
 using System.Diagnostics;
 using System.Collections.Generic;
 using static SharpUp.Utilities.FileUtils;
 using System.Security.Principal;
 using static SharpUp.Native.Win32;
+using System.Security.AccessControl;
 
 namespace SharpUp.Checks
 {
@@ -38,18 +40,17 @@ namespace SharpUp.Checks
         }
 
         public ProcessDLLHijack()
-		{
+		      {
             // TODO: Take argements and add them to an array for scoping
             //string[] options = args;
 
             // Registry key where known DLLs are listed
             string[] keyname = GetRegSubkeys("HKLM", _regPath);
 
-
             // Create List for DLL values
             List<string> Dlls = new List<string>();
 
-            //Get the value for each of the values and add it to the list
+            // Get the value for each of the values and add it to the list
             foreach (string valuename in keyname)
             {
                 string dllname = valuename.ToLower();
@@ -64,28 +65,43 @@ namespace SharpUp.Checks
                 // Try to check the modules loaded for the process
                 try
                 {
-                    Console.WriteLine("[+] Checking modules for {0}", process.ProcessName);
-                    var processmodules = process.Modules;
-
                     // Go through each module loaded in the process
+                    var processmodules = process.Modules;
                     foreach (ProcessModule module in processmodules)
                     {
                         string modules = module.ModuleName;
                         modules = modules.ToLower();
                         string filepath = module.FileName.ToLower();
-
-                        // Exclude items that do not end with .dll, exclude known dlls, exclude items in c:\\windows\\system32
-                        if (module.FileName.EndsWith(".dll") && !Dlls.Contains(modules) && !filepath.Contains("c:\\windows"))
+                        bool writeperms = CheckAccess(filepath, FileSystemRights.Write);
+                        // Exclude items that do not end with .dll, exclude known dlls, exclude items in c:\\windows, exclude files that do not have the correct permissions
+                        if (module.FileName.EndsWith(".dll") && !Dlls.Contains(modules) && !filepath.Contains("c:\\windows") && writeperms == false)
                         {
-                            // Final output for full path to DLLs that meet the parameters
-                            Console.WriteLine("[+] Hijackable DLL: {0} ", module.FileName.ToString());                            
+                            if (filepath.Contains("c:\\program files"))
+                            {
+                                Console.WriteLine("[+] Potenatially Hijackable DLL: {0}\n" +
+                                    "[!] Files in C:\\Program Files and C:\\Program Files (x86) may be false positives. Permissions should be verified manually.\n" +
+                                    "[+] Associated Process is {1} with PID {2}",
+                                    module.FileName.ToString(),
+                                    process.ProcessName.ToString(),
+                                    process.Id.ToString());
+                            }
+                            else
+                            {
+                                Console.WriteLine("[+] Hijackable DLL: {0}\n" +
+                                    "[+] Associated Process is {1} with PID {2} ",
+                                    module.FileName.ToString(),
+                                    process.ProcessName.ToString(),
+                                    process.Id.ToString());
+                            }
                         }
                     }
                 }
                 catch
                 {
                     // Output for when the current user doesn't have permissions for a process
-                    Console.WriteLine("[-] Access denied for {0} under PID {1}", process.ProcessName.ToString(), process.Id.ToString());
+                    // Console.WriteLine("[-] Access denied for {0} under PID {1}", process.ProcessName.ToString(), process.Id.ToString());
+                    continue;
+
                 }
             }
         }
